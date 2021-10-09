0c9e7097ac362d6c40751cfe0376fd3e796b7730
Administrator <admin@example.com>
2021-10-09 17:18:23 +0200

    1. Valida nuevos clientes

---
+++ js/nuevocliente.js
@@ -0,0 +1,31 @@
+(function() {
+    const formulario = document.querySelector('#formulario');
+    formulario.addEventListener('submit', validarCliente);
+
+    function validarCliente(e) {
+        e.preventDefault();
+
+        const nombre = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa = document.querySelector('#empresa').value;
+    
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa
+        }
+
+        if (validar(cliente)) {
+            //Mostrar mensaje
+            console.log('Todos los campos son obligatorios');
+            return;
+        }
+        console.log('Se pasó la validación');
+    }
+
+    function validar(obj) {
+        return !Object.values(obj).every(input => input !== '');
+    }
+})();
