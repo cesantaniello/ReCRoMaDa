78b7bd4387f29ac8a7697cdbb5ca84ca38c0d112
Administrator <admin@example.com>
2021-10-10 12:20:42 +0200

    4. Lista clientes de la API

--- js/API.js
+++ js/API.js
@@ -1,5 +1,6 @@
 const url = 'http://localhost:4000/clientes';
 
+//Cuando se crea un nuevo cliente
 export const nuevoCliente = async cliente => {
     try {
         await fetch(url, {
@@ -12,5 +13,16 @@ export const nuevoCliente = async cliente => {
         window.location.href = 'index.html';
     } catch (error) {
         console.log(error)
+    }
+}
+
+//Obtiene todos los clientes
+export const obtenerClientes = async() => {
+    try {
+        const resultado = await fetch(url);
+        const clientes = await resultado.json();
+        return clientes;
+    } catch (error) {
+        console.log(error);
     }
 }
