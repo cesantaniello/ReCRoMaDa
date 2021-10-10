7cdf451a3a634fd8c212175f36f030242c34717d
Administrator <admin@example.com>
2021-10-10 12:45:08 +0200

    5. Elimina clientes de la API

--- js/API.js
+++ js/API.js
@@ -26,3 +26,14 @@ export const obtenerClientes = async() => {
         console.log(error);
     }
 }
+
+//Elimina cliente
+export const eliminarCliente = async id => {
+    try {
+        await fetch(`${url}/${id}`, {
+            method: 'DELETE'
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
-import {obtenerClientes} from "./API.js";
+import {obtenerClientes, eliminarCliente} from "./API.js";
 
 (function() {
     const listado = document.querySelector('#listado-clientes');
 
     document.addEventListener('DOMContentLoaded', mostrarClientes);
+
+    listado.addEventListener('click', confirmarEliminar);
 
     async function mostrarClientes() {
         const clientes = await obtenerClientes();
@@ -32,4 +34,15 @@ import {obtenerClientes} from "./API.js";
             listado.appendChild(row);
         });
     }
+
+    function confirmarEliminar(e) {
+        if(e.target.classList.contains('eliminar')){
+            const clienteId = parseInt(e.target.dataset.cliente);
+            const confirmar = confirm('¿Deseas eliminar este registro?')
+            if(confirmar){
+                eliminarCliente(clienteId)
+            }
+        }
+    }
+
 })();
